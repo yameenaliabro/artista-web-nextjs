ce69b4a7a4c066b6a9a7049e6a01138dceb1de5d
Shayan Fareed <sfx.deve@gmail.com>
2025-07-22 17:22:14 +0500

    feat: add Container component for layout structure and update Header to use Container

---
+++ src/app/_layout/container.tsx
@@ -0,0 +1,11 @@
+import type { PropsWithChildren } from "react";
+
+import { cn } from "~/lib/utils";
+
+interface ContainerProps extends PropsWithChildren {
+	className?: string;
+}
+
+export function Container({ children, className }: ContainerProps) {
+	return <div className={cn("max-w-7xl mx-auto", className)}>{children}</div>;
+}
--- src/app/_layout/header.tsx
+++ src/app/_layout/header.tsx
@@ -1,3 +1,20 @@
+import { Container } from "~/app/_layout/container";
+import { cn } from "~/lib/utils";
+
 export function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<div className={cn("bg-primary text-primary-foreground text-center p-2")}>
+				<Container>
+					<p>Stylish Blinds, Timeless Design, Comfort Guaranteed</p>
+				</Container>
+			</div>
+			<nav>
+				<Container className={"flex justify-between items-center px-2 py-4"}>
+					<h1>Logo</h1>
+					<ul></ul>
+				</Container>
+			</nav>
+		</header>
+	);
 }
