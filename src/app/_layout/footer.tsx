d05d10f7fb8886f18c71437e7d3fd8ab5e7a422e
Shayan Fareed <sfx.deve@gmail.com>
2025-07-23 23:50:22 +0500

    footer copy and terms etc links

--- src/app/_layout/footer.tsx
+++ src/app/_layout/footer.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+
+import { cn } from "~/lib/utils";
+import { Container } from "./container";
+
 export function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<div className={cn("bg-primary-1000 text-primary-foreground py-8")}>
+				<Container className="flex items-center justify-between gap-6">
+					<p>Copyright ⓒ ArtistaBrands, 2025. All rights reserved.</p>
+					<nav>
+						<ul className="flex items-center gap-6">
+							<li>
+								<Link href="#" className="border-b-[0.5px] text-sm">
+									Terms
+								</Link>
+							</li>
+							<li>
+								<Link href="#" className="border-b-[0.5px] text-sm">
+									Privacy Policy
+								</Link>
+							</li>
+							<li>
+								<Link href="#" className="border-b-[0.5px] text-sm">
+									Cookies Policy
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</Container>
+			</div>
+		</footer>
+	);
 }
